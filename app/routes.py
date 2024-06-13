a0a4438ebe4e90f187a140bb798a87353e8a46f7
skam12345 <skam1234561@gmail.com>
2024-06-14 01:47:45 +0900

    총 페이지 로직 변경

--- app/routes.py
+++ app/routes.py
@@ -158,6 +158,8 @@ def read_paging_poem():
             
             if math.ceil(number_all_poem / POEM_LIMIT) == 0:
                 all_count_page += 1
+            if number_all_poem % POEM_LIMIT < 10:
+                all_count_page += 1
             if all_count_page - (VIEW_PAGE_LIMIT - 1) <= 0:
                 first_paging = 1
             else :
